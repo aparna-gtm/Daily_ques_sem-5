8b173a0d68d17f233f0f92bef85367fce7a994cf
aparna-gtm <aparnagautam3323@gmail.com>
2025-08-01 21:49:04 +0530

    first ques

---
+++ Binary_Tree_Maximum_Path_Sum.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    static int max=Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        max=Integer.MIN_VALUE;
+        sum(root);
+        return max;
+
+    }
+    public int sum(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        int left=Math.max(0,sum(root.left));
+        int right=Math.max(0,sum(root.right));
+
+
+        int currMax= root.val+left+right;
+
+        max=Math.max(currMax,max);
+
+        return root.val+Math.max(left,right);
+    }
+}
