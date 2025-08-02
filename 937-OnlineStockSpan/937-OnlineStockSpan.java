97c1730bb43e264de9b75a72ba094eeb8f3d19c4
Aparna Gautam <138388004+aparna-gtm@users.noreply.github.com>
2025-08-02 15:19:20 +0530

    Create 937-OnlineStockSpan

---
+++ 937-OnlineStockSpan/937-OnlineStockSpan.java
@@ -0,0 +1,36 @@
+// Last updated: 8/2/2025, 3:19:21 PM
+class StockSpanner {
+    private Stack<Integer> st;
+    private ArrayList<Integer> ll;
+    int idx;
+    public StockSpanner() {
+        st=new Stack<>();
+        ll=new ArrayList<>();
+        idx=0;
+    }
+    
+    public int next(int price) {
+        ll.add(price);
+        while(!st.isEmpty() && ll.get(st.peek())<=price){
+            st.pop();
+
+        }
+        int span;
+        if(st.isEmpty()){
+            span=idx+1;
+        }
+        else{
+            span=idx-st.peek();
+        }
+        st.push(idx);
+        idx++;
+        return span;
+
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
