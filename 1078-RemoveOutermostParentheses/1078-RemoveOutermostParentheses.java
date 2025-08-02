f6b2c4af21d9b0747cbb4927832fa43ae0670ead
Aparna Gautam <138388004+aparna-gtm@users.noreply.github.com>
2025-08-02 15:18:40 +0530

    Create 1078-RemoveOutermostParentheses

---
+++ 1078-RemoveOutermostParentheses/1078-RemoveOutermostParentheses.java
@@ -0,0 +1,23 @@
+// Last updated: 8/2/2025, 3:18:41 PM
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder sb=new StringBuilder();
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                if(count>0){
+                    sb.append('(');
+                }
+                count++;
+            }
+            else{
+                count--;
+                if(count>0){
+                    sb.append(')');
+                }
+
+            }
+        }
+        return sb.toString();
+    }
+}
