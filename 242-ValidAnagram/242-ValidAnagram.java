20868af6623b497e3052f6b257b7b1bd0551dc45
Aparna Gautam <138388004+aparna-gtm@users.noreply.github.com>
2025-08-02 15:21:55 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,23 @@
+// Last updated: 8/2/2025, 3:21:56 PM
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int[] freq=new int[26];
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+            
+        }
+        for(int i=0;i<t.length();i++){
+            freq[t.charAt(i)-'a']--;
+            
+        }
+        for(int i : freq){
+            if(i!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
