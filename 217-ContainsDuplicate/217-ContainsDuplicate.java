009c7d03ce213d227db46cf2483284bd62518430
Aparna Gautam <138388004+aparna-gtm@users.noreply.github.com>
2025-08-02 15:22:17 +0530

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -0,0 +1,13 @@
+// Last updated: 8/2/2025, 3:22:18 PM
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+}
