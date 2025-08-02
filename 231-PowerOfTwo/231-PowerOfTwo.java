c002a114225eb690817341cb16428651795c0568
Aparna Gautam <138388004+aparna-gtm@users.noreply.github.com>
2025-08-02 15:22:07 +0530

    Create 231-PowerOfTwo

---
+++ 231-PowerOfTwo/231-PowerOfTwo.java
@@ -0,0 +1,23 @@
+// Last updated: 8/2/2025, 3:22:08 PM
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<0){
+            return false;
+        }
+        if(n==0){
+            return false;
+        }
+        if(n==1){
+            return true;
+        }
+        
+        while(n>1){
+            if(n%2!=0){
+             //   System.out.println(n);
+                return false;
+            }
+            n/=2;
+        }
+        return true;
+    }
+}
