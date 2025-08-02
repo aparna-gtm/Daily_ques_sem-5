6f93b3a1eb11d812696fbf9b0fc959313e41864f
Aparna Gautam <138388004+aparna-gtm@users.noreply.github.com>
2025-08-02 15:21:11 +0530

    Create 389-FindTheDifference

---
+++ 389-FindTheDifference/389-FindTheDifference.java
@@ -0,0 +1,19 @@
+// Last updated: 8/2/2025, 3:21:12 PM
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int[] freq=new int[26];
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+        }
+         for(int i=0;i<t.length();i++){
+            freq[t.charAt(i)-'a']--;
+        }
+
+         for(int i=0;i<26;i++){
+            if(freq[i]==-1){
+                return (char)(i+'a');
+            }
+        }
+        return 'a';
+    }
+}
