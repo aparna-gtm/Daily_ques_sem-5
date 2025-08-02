bc754f8161e0d67df764af28ccf4cc3aa345eca1
Aparna Gautam <138388004+aparna-gtm@users.noreply.github.com>
2025-08-02 15:21:47 +0530

    Create 275-HIndexIi

---
+++ 275-HIndexIi/275-HIndexIi.java
@@ -0,0 +1,23 @@
+// Last updated: 8/2/2025, 3:21:48 PM
+class Solution {
+    public int hIndex(int[] citations) {
+        if (citations.length == 1) {
+            return citations[0] >= 1 ? 1 : 0; // Special case for a single element
+        }
+        int start=0;
+        int end=citations.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(citations.length-mid==citations[mid]){
+                return citations[mid];
+            }
+            else if(citations.length-mid>citations[mid]){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return citations.length - start;
+    }
+}
