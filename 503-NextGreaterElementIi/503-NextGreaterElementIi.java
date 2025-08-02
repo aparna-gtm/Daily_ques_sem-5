61787c90bbc83418ad3deb5a58b310c6d9a3c7e7
Aparna Gautam <138388004+aparna-gtm@users.noreply.github.com>
2025-08-02 15:20:34 +0530

    Create 503-NextGreaterElementIi

---
+++ 503-NextGreaterElementIi/503-NextGreaterElementIi.java
@@ -0,0 +1,20 @@
+// Last updated: 8/2/2025, 3:20:35 PM
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        Stack<Integer> st = new Stack<>();
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1); // Default value if no greater element exists
+
+        for (int i = 0; i < 2 * n; i++) {
+            int idx = i % n;
+            while (!st.isEmpty() && nums[idx] > nums[st.peek()]) {
+                ans[st.pop()] = nums[idx];
+            }
+            if (i < n) {
+                st.push(idx);
+            }
+        }
+        return ans;
+    }
+}
