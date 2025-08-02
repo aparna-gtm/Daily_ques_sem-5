1e37970916d3cbabca11279f90dde27a5d8c075c
Aparna Gautam <138388004+aparna-gtm@users.noreply.github.com>
2025-08-02 15:18:22 +0530

    Create 1341-SplitAStringInBalancedStrings

---
+++ 1341-SplitAStringInBalancedStrings/1341-SplitAStringInBalancedStrings.java
@@ -0,0 +1,22 @@
+// Last updated: 8/2/2025, 3:18:23 PM
+class Solution {
+    public int balancedStringSplit(String s) {
+        int l=0;
+        int r=0;
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='L'){
+                l++;
+            }
+            if(s.charAt(i)=='R'){
+                r++;
+            }
+            if(l==r){
+                count++;
+                l=0;
+                r=0;
+            }
+        }
+        return count;
+    }
+}
